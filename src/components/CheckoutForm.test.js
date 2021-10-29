2f7c442badabbc92c53ded698246cae2487ab22b
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-10-29 12:21:23 -0500

    Finishing up checkoutForm Tests

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -2,34 +2,44 @@ import React from "react";
 import MutationObserver from 'mutationobserver-shim';
 import { render, screen, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
-import App from '../App'
+
 import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("renders without errors", () => {
-    render(<App/>);
+    render(<CheckoutForm/>);
 });
 
-test("shows success message on submit with form details", () => {
-    render(<App/>);
+test("shows success message on submit with form details", async () => {
+    render(<CheckoutForm/>);
 
-    const Firstname = screen.getByText(/firstname/i);
+    const Firstname = screen.getByLabelText("First Name:");
     userEvent.type(Firstname, "Billy");
 
-    const Lastname = screen.getByText(/lastname/i);
+    const Lastname = screen.getByLabelText("Last Name:");
     userEvent.type(Lastname, "TheKid");
 
-    const Address = screen.getByText(/address/i);
-    userEvent.type(Address, "123 WildWest Street")
+    const Address = screen.getByLabelText("Address:");
+    userEvent.type(Address, "123 WildWest Street");
 
-    const State = screen.getByText(/state/i);
+    const City = screen.getByLabelText("City:");
+    userEvent.type(City, "outlaw");
+
+    const State = screen.getByLabelText("State:");
     userEvent.type(State, "Arizona");
 
-    const zip = screen.getByText(/zip/i);
+    const zip = screen.getByLabelText("Zip:");
     userEvent.type(zip, "12345");
 
     const button = screen.findByRole("button");
     userEvent.click(button);
-    
+
+    await waitFor(() => {
+        const output = screen.queryByText(/You have ordered some plants! Woo-hoo! 🎉 Your new green friends will be shipped to:/i);
+        expect(output).toBeInTheDocument();
+
+        const orderinfo = screen.queryByText(/Billy TheKid 123 WildWest Street Outlaw, Arizona 12345/i);
+        expect(orderinfo).toBeInTheDocument();
+    })
 });
